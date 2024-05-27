433a21c5b1c5d261a36572e4de885a7e58c2212c
Robert Santos <robertdahora@gmail.com>
2024-05-26 21:10:32 -0300

    add migrations

---
+++ server/prisma/migrations/20240526193732_create_cars/migration.sql
@@ -0,0 +1,11 @@
+-- CreateTable
+CREATE TABLE "cars" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "name" TEXT NOT NULL,
+    "brand" TEXT NOT NULL,
+    "model" TEXT NOT NULL,
+    "year" INTEGER NOT NULL,
+    "mileage" INTEGER NOT NULL,
+    "transmission" TEXT NOT NULL,
+    "photo" TEXT NOT NULL
+);
